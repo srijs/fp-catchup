571d046356d9bb8d6f3a4cef1beb243e904fcc3b
Sam Rijs <srijs@airpost.net>
2015-06-18 11:29:02 +1000

    add monoid example

---
+++ 02-monoid.hs
@@ -0,0 +1,14 @@
+module Monoid where
+
+import Data.Monoid (Monoid, mempty, mappend)
+
+{- Monoid Laws
+
+- associativity: mappend (mappend a b) c == mappend a (mappend b c)
+- neural element: mappend mempty a == mappend a mempty == a
+
+-}
+
+instance Monoid Int where
+  mempty = 0
+  mappend = (+)

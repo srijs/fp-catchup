406a46d7c9e68dbca0df45faed37b8ce9210f004
Sam Rijs <srijs@airpost.net>
2015-06-18 14:24:56 +1000

    add FP05 executable

---
+++ FP05.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+
+import Data.ByteString (ByteString)
+import Network.HTTP.Conduit
+import Data.Aeson
+import Data.Time.Clock (UTCTime)
+import Data.Text (Text)
+import GHC.Generics
+
+data Repo = Repo
+  { name :: Text
+  , description :: Text
+  , created_at :: UTCTime
+  , language :: Maybe Text
+  } deriving (Show, Generic)
+
+instance FromJSON Repo
+
+repos :: String -> IO (Either String [Repo])
+repos name = do
+  initReq <- parseUrl $ "https://api.github.com/users/" ++ name ++ "/repos"
+  let req = initReq { requestHeaders = [("User-Agent", "FP-Catchup")] }
+  res <- withManager $ httpLbs req
+  return $ eitherDecode $ responseBody res
+
+main :: IO ()
+main = do
+  rs <- repos "srijs"
+  case rs of
+    Left err -> putStrLn err
+    Right rs'  -> mapM_ print $ take 5 rs'

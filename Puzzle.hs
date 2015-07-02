e8cb3785d3f79865227e9e7de55e0a5e0a70b303
Sam Rijs <srijs@airpost.net>
2015-07-02 15:50:47 +1000

    add puzzle

---
+++ Puzzle.hs
@@ -0,0 +1,12 @@
+module Puzzle where
+
+greeting :: String -> String
+greeting s = "Hello, " ++ s ++ "!"
+
+-- getLine  :: IO String
+-- putStrLn :: String -> IO ()
+-- (.)      :: (b -> c) -> (a -> b) -> a -> c
+-- (>>=)    :: Monad m => m a -> (a -> m b) -> m b
+
+main :: IO ()
+main = undefined
